bc383a2867abf141acf1c0d98afca4931c62797d
Oleksandr bc2025-6 <sashasrogiy3@gmail.com>
2025-11-18 12:25:50 +0200

    update main.js

--- main.js
+++ main.js
@@ -104,7 +104,6 @@ const upload = multer({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("."));
 
 app.use((error, req, res, next) => {
   if (error instanceof multer.MulterError) {
